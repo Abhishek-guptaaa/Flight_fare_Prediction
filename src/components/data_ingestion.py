53f6e60aaea8c72060d141d664737624a9da47c3
Abhishek <101599136+Abhishek-guptaaa@users.noreply.github.com>
2024-08-26 16:32:06 +0530

    data ingestion completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,15 +1,19 @@
 import os
 import sys
-from src.exception.exception import CustomException
-from src.logger.logger import logging
 import pandas as pd
-from mongo import read_mongo_data
+import numpy as np
+from src.logger.logger import logging
+from src.exception.exception import CustomException
+from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from mongo import read_mongo_data
 
 @dataclass
 class DataIngestionConfig:
-    raw_data_path: str = os.path.join("artifacts", "raw.csv")
+    raw_data_path:str=os.path.join("artifacts","raw.csv")
+    train_data_path:str=os.path.join("artifacts","train.csv")
+    test_data_path:str=os.path.join("artifacts","test.csv")
+
 
 class DataIngestion:
     def __init__(self):
@@ -28,20 +32,28 @@ class DataIngestion:
                 logging.error("No data found to ingest")
                 return None
 
-            logging.info("Reading completed from MongoDB")
+            os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info(" i have saved the raw dataset in artifact folder")
+            
+            logging.info("here i have performed train test split")
+            
+            train_data,test_data=train_test_split(df,test_size=0.25)
+            logging.info("train test split completed")
+            
+            train_data.to_csv(self.ingestion_config.train_data_path,index=False)
+            test_data.to_csv(self.ingestion_config.test_data_path,index=False)
+            
+            logging.info("data ingestion part completed")
+            
+            return (
+                 
+                
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
 
-            # Create directory if it doesn't exist
-            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
-
-            # Save raw data to CSV
-            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
-
-            logging.info("Data Ingestion is completed")
-            return self.ingestion_config.raw_data_path
         except Exception as e:
-            raise CustomException(e, sys)
+            logging.info()
+            raise CustomException(e,sys)
 
-
-
-
-        
